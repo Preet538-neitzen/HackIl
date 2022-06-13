9179b19b8b1c356184f089880f45769206346758
Preet Sanghavi <54236288+Preet538-neitzen@users.noreply.github.com>
2022-06-13 15:26:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>HackIllinois</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+		<Leaderboard />
+      </div>
+    );
+  }
+}
+
+class Leaderboard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { dataReceived: [], value: 0 };
+		// this.props.data.dataReceived = [];
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	// on load, get the information
+	//state.dataReceived = [];
+
+	// getInitialState() {
+	// 	return {
+	// 		dataReceived: []
+	// 	}
+	// }
+
+	handleChange(event) {
+		this.setState({value: event.target.value});
+		this.serverRequest = axios.get('https://api.hackillinois.org/profile/leaderboard/', {params:{limit : event.target.value }})
+		.then((result) => {
+			this.setState({
+				dataReceived: result.data.profiles
+			});
+		});
+	  }
+	
+	handleSubmit(event) {
+		alert('A value was submitted: ' + this.state.value);
+		event.preventDefault();
+	  }
+
+
+
+	// componentWillUnmount() {
+	// 	this.serverRequest.abort();
+	// }
+
+
+	render() {
+		// var campers = this.props.data.map(function(camper) {
+		// 	return <Camper />;
+		// });
+		// {campers}
+		return(
+
+		<div>
+		<form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+			
+		<table className="Leaderboard">
+
+			<tr>
+				<td>Leaderboard</td>
+			</tr>
+
+			<tr>
+				{this.state.dataReceived.map(function(camper) {
+					return (
+						<tr className="Camper">
+							<td>Username: {camper.discord}</td>
+							<td>Points: {camper.points}</td>
+						</tr>
+					);
+				})
+				}
+			</tr>
+		</table>
+		</div>
+		)
+	}
+}
+
+class Camper extends Component {
+	render() {
+		return <tr>
+			<td className="number"></td>
+			<td className="name"></td>
+			<td className="points30"></td>
+			<td className="pointsAll"></td>
+		</tr>
+	}
+}
+
+export default App;
